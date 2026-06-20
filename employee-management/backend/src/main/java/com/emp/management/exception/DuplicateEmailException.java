934ba3254d3841c4cf6d7adae52bad4251997394
ASURYA23CSB49 <suryafranky007@gmail.com>
2026-06-20 20:55:47 +0530

    final

--- employee-management/backend/src/main/java/com/emp/management/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.emp.management.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String email) {
-        super("Employee already exists with email: " + email);
-    }
-}
